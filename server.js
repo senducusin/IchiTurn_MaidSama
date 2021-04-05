9a1c9996e4289fabc0592d8246dbcdce075e7702
Jansen Ducusin <senducusin@gmail.com>
2021-04-05 14:56:03 +0800

    Updated port and script

--- server.js
+++ server.js
@@ -28,6 +28,5 @@ app.post("/api/send-codes",(req,res)=>{
     }
 })
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000")
-})
+const port = process.env.PORT || 3000
+app.listen(port, ()=> console.log(`Listening on port ${port}...`))
