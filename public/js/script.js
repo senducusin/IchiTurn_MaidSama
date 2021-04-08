ac1adff5824c6ace2bf9adae5323d034a0f23648
Jansen Ducusin <senducusin@gmail.com>
2021-04-08 10:03:45 +0800

    revised to accept Card Name or Passcode instead of Card-Code, added API requests

--- public/js/script.js
+++ public/js/script.js
@@ -1,94 +1,80 @@
 (function () {
-    
-    let localStorageKey = "ICHITURN_LOCALSTORAGE_ORDERS"
 
-    window.addEventListener("load", function(){
+    let localStorageKey = "ICHITURN_LOCALSTORAGE_ORDERS_V2.1"
+
+    window.addEventListener("load", function () {
         // collect from browser storage
-        let savedCardCodes = getCardCodes()
+        let savedCards = getCardsFromStorage()
 
         // display to collection
-        for (index in savedCardCodes) {
-            let cardCode = savedCardCodes[index]
-            appendNewCardCode(cardCode)
+        for (index in savedCards) {
+            let card = savedCards[index]
+            appendNewCardElement(card)
         }
     })
 
-    document.getElementById("submit-card-codes").addEventListener("click", function(){
-        if (validateEmailAddressValue()){
+    document.getElementById("submit-card-codes").addEventListener("click", function () {
+        if (validateEmailAddressValue()) {
             hideNotification()
-            
+
             submit()
-        }else{
+        } else {
             showNotification("error", "Invalid email address")
         }
     })
 
-    document.getElementById("clear-notification").addEventListener("click",function(){
+    document.getElementById("clear-notification").addEventListener("click", function () {
         hideNotification()
     })
 
-    document.getElementById("add-card-code").addEventListener("click",function(){
-        let cardCode = document.getElementById("card-code-input").value.toUpperCase()
+    document.getElementById("add-card-code").addEventListener("click", function () {
+        let cardCode = document.getElementById("card-input").value
 
-        if (validateCardCodeValue(cardCode)) {
-            addNewCardCode(cardCode)
-        }else{
-            showNotification("error", "Invalid card code format. Valid format must be 10 characters in length, must contain '-', and from OCG set")
-        }
-        
+        addNewCardCode(cardCode)
     })
 
     // HELPERS
-    function clearCardCodes(){
+    function clearCards() {
         localStorage.setItem(localStorageKey, "[]")
     }
 
-    function clearUI(){
+    function clearUI() {
         document.querySelector(".collection").innerHTML = ""
         document.getElementById("email-address-input").value = ""
     }
 
-    function submit(){
-        const params = {
-            cardCodes: getCardCodes(),
+    function submit() {
+
+        const parameters = {
+            cards: getCardsFromStorage(),
             emailAddress: document.getElementById("email-address-input").value
         };
 
         (async () => {
             const rawResponse = await fetch('/api/send-codes', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(params)
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(parameters)
             });
 
             const response = await rawResponse.json();
 
-            if (response){
-                if (response.status == "ok"){
+            if (response) {
+                if (response.status == "ok") {
                     showNotification("success", "Request submitted. A quote will be sent to your email shortly.")
-
-                    clearCardCodes()
-                    clearUI()
+                    console.log(response)
+                    // clearCards()
+                    // clearUI()
                 }
             }
         })()
+
     }
 
-    function validateCardCodeValue(cardCode){
-
-        if (cardCode.length != 10) return false
-
-        if (cardCode.indexOf("-") != 4) return false
-
-        if (cardCode.indexOf("JP") != 5) return false
-
-        return true
-    }
-
-    function validateEmailAddressValue(){
+    function validateEmailAddressValue() {
         let emailAddress = document.getElementById("email-address-input").value
 
         if (emailAddress.length == 0) return false
@@ -100,58 +86,102 @@
         return false
     }
 
-    function addNewCardCode(cardCode){
-        let savedCardCodes = getCardCodes()
-        if (savedCardCodes.length == 0 || savedCardCodes.indexOf(cardCode) == -1) {
-            savedCardCodes.push(cardCode)
-            localStorage.setItem(localStorageKey, JSON.stringify(savedCardCodes))
-            appendNewCardCode(cardCode)
-            hideNotification()
-            document.getElementById("card-code-input").value = ""
-        }else{
+    function addNewCardCode(cardKeyword) {
+        let savedCards = getCardsFromStorage()
+
+        if (savedCards.length == 0 || savedCards.indexOf(cardKeyword) == -1) {
+
+            const parameters = {
+                card: cardKeyword
+            };
+
+            (async () => {
+                const rawResponse = await fetch('/api/add-card', {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(parameters)
+                });
+
+                const response = await rawResponse.json();
+
+                if (response) {
+                    if (response.status == "ok") {
+                        let card = response.card
+
+                        if (response.hasOwnProperty("card")) {
+                            savedCards.push(card)
+                            localStorage.setItem(localStorageKey, JSON.stringify(savedCards))
+                            appendNewCardElement(card)
+                            scrollCardsToBottom()
+                            hideNotification()
+                            clearInput()
+                        }
+
+                    } else {
+                        showNotification("error", response.error)
+                    }
+                }
+            })()
+        } else {
             showNotification("error", "Card code is already in the list")
         }
     }
 
-    function removeCardCode(cardCodeToRemove){
-        let savedCardCodes = getCardCodes()
-        
-        savedCardCodes = savedCardCodes.filter(cardCode => cardCode != cardCodeToRemove)
-        localStorage.setItem(localStorageKey, JSON.stringify(savedCardCodes))
+    function clearInput() {
+        document.getElementById("card-input").value = ""
     }
 
-    function hideNotification(){
+    function scrollCardsToBottom() {
+        let parentElementCollection = document.querySelector(".collection")
+        parentElementCollection.scrollTo(0, parentElementCollection.scrollHeight);
+    }
+
+    function removeCardCodeWithPasscode(passcode) {
+        let savedCards = getCardsFromStorage()
+
+        savedCards = savedCards.filter(card => card.passcode != passcode)
+
+        localStorage.setItem(localStorageKey, JSON.stringify(savedCards))
+    }
+
+    function hideNotification() {
         let notificationContainer = document.getElementById("notification")
         notificationContainer.classList.add("hidden")
     }
 
-    function showNotification(type, message){
+    function showNotification(type, message) {
         let notificationContainer = document.getElementById("notification")
 
         let notificationText = document.getElementById("notification-text")
-        
+
         let color = type == "error" ? "red" : "teal"
 
         notificationText.innerText = message
-        
+
         notificationContainer.classList.add(color)
         notificationContainer.classList.remove("hidden")
     }
 
-    function appendNewCardCode(cardCode){
-        let cardCodeLowerCased = cardCode.toLowerCase()
-
+    function appendNewCardElement(card) {
         let listItem = document.createElement("li")
-        listItem.id = `list-${cardCodeLowerCased}`
-        listItem.classList.add("collection-item","row")
+        listItem.id = `list-${card.passcode}`
+        listItem.classList.add("collection-item", "row")
 
-        let divCardCode = document.createElement("div")
-        divCardCode.classList.add("card-code", "col", "s8")
-        divCardCode.innerText = cardCode
-        listItem.appendChild(divCardCode)
+        let divCardPasscode = document.createElement("div")
+        divCardPasscode.classList.add("card-passcode", "col", "s2")
+        divCardPasscode.innerText = card.passcode
+        listItem.appendChild(divCardPasscode)
+
+        let divCardName = document.createElement("div")
+        divCardName.classList.add("card-name", "col", "s6")
+        divCardName.innerText = card.name
+        listItem.appendChild(divCardName)
 
         let divInputField = document.createElement("div")
-        divInputField.classList.add("input-field","col","s4", "flexy")
+        divInputField.classList.add("input-field", "col", "s4", "flexy")
         listItem.appendChild(divInputField)
 
         let divSpacer = document.createElement("div")
@@ -160,13 +190,13 @@
 
         let button = document.createElement("a")
         button.classList.add("waves-effect", "waves-light", "btn", "red")
-        button.id = `button-${cardCodeLowerCased}`
+        button.id = `button-${card.passcode}`
         button.innerText = "Remove"
 
-        button.addEventListener("click",function(){
+        button.addEventListener("click", function () {
             let parentElementCollection = document.querySelector(".collection")
-            const cardCode = this.id.replace("button-","").toUpperCase()
-            removeCardCode(cardCode)
+            const passcode = this.id.replace("button-", "").toUpperCase()
+            removeCardCodeWithPasscode(passcode)
             parentElementCollection.removeChild(listItem)
         })
 
@@ -176,7 +206,7 @@
         parentElementCollection.appendChild(listItem)
     }
 
-    function getCardCodes(){
+    function getCardsFromStorage() {
         let savedOrdersString = localStorage.getItem(localStorageKey)
 
         let value = JSON.parse(savedOrdersString)
--- server.js
+++ server.js
@@ -4,74 +4,137 @@ const axios = require('axios')
 
 const app = express()
 
-app.use("/public",express.static("public"))
+app.use("/public", express.static("public"))
 app.use(express.json())
 
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
     res.render("index")
 })
 
-app.post("/api/send-codes",(req,res)=>{
+app.post("/api/add-card", (req, res) => {
     const body = req.body
 
-    if(body.hasOwnProperty("cardCodes") === true){
-        res.status(200).send({status:"ok"})
-        // Logs
-        console.log(body) 
+    if (body.hasOwnProperty("card")) {
 
-        // send axios request here
-    }else{
+        let card = body.card
+        let propertyName = (card.length == 8 && /[0-9]/.test(card)) ? 'id' : 'name'
+
+        axios({
+            method: "get",
+            url: `https://db.ygoprodeck.com/api/v7/cardinfo.php?${propertyName}=${card}`,
+        }).then(function (response) {
+
+            let data = response.data.data[0]
+            let id = data.id
+            let name = data.name
+
+            res.status(200).send({
+                status: "ok",
+                card: {
+                    passcode: id,
+                    name: name
+                }
+            })
+
+        }).catch(function (error) {
+            let errorMessage = error.response.data.error
+            errorMessage = errorMessage.split(". ")
+
+            res.status(200).send({
+                status: "error",
+                error: `${errorMessage[0]}.`
+            })
+        })
+
+    } else {
         res.status(400).send("Invalid Request")
     }
 })
 
-// 50371210
+app.post("/api/send-codes", (req, res) => {
+    const body = req.body
 
-let cardCodes = []
+    if (body.hasOwnProperty("cards")) {
 
-fetchSet(46986414, "")
-// console.log(cardCodes)
+        let passcodes = []
 
-function fetchSet(passcode, rdcontinue = ""){
-    axios({
-        method: "get",
-        url: `https://yugioh.fandom.com/api.php?action=query&prop=redirects&redirects&continue&format=json&titles=${passcode}&rdcontinue=${rdcontinue}`,
-    }).then(function (response){
-        let pages = response.data.query.pages
-        let continueRedirect
-        // = response.data.continue.rdcontinue
-        if (response.data.hasOwnProperty("continue")){
-            if(response.data.continue.hasOwnProperty("rdcontinue")){
-                continueRedirect = response.data.continue.rdcontinue
-            }
+        let email = body.emailAddress
+        let cards = body.cards
+
+        for (index in cards) {
+            passcodes.push(cards[index].passcode)
         }
 
-        for (var page in pages){
-            let redirects = pages[page].redirects
-            for (var index in redirects){
-                let setTitle = redirects[index].title
-                if( /[A-Z0-9]+-JP[0-1]+/.test(setTitle) || 
-                    /[A-Z0-9]+-JA[0-1]+/.test(setTitle) || 
-                    /[A-Z0-9]+-AE[0-1]+/.test(setTitle) || 
-                    /[A-Z0-9]+-[0-1]+/.test(setTitle)){
-                        cardCodes.push(setTitle)
+        fetchSets(passcodes)
+
+    } else {
+        res.status(400).send("Invalid Request")
+    }
+
+    async function fetchSets(sets) {
+        let cardCodes = []
+        let setCounter = 0
+
+        for (setIndex in sets) {
+            let passcode = sets[setIndex]
+            await fetchSet(passcode, "")
+        }
+
+        function fetchSet(passcode, rdcontinue = "") {
+            axios({
+                method: "get",
+                url: `https://yugioh.fandom.com/api.php?action=query&prop=redirects&redirects&continue&format=json&titles=${passcode}&rdcontinue=${rdcontinue}`,
+            }).then(function (response) {
+                let pages = response.data.query.pages
+                let continueRedirect
+
+                if (response.data.hasOwnProperty("continue")) {
+                    if (response.data.continue.hasOwnProperty("rdcontinue")) {
+                        continueRedirect = response.data.continue.rdcontinue
+                    }
                 }
-            }
+
+                for (var page in pages) {
+                    let redirects = pages[page].redirects
+                    for (var index in redirects) {
+                        let setTitle = redirects[index].title
+                        if (codeIsFromOcgSet(setTitle)) {
+                            cardCodes.push(setTitle)
+                        }
+                    }
+                }
+
+                if (continueRedirect != undefined) {
+                    fetchSet(passcode, continueRedirect)
+                } else {
+
+                    setCounter++
+                    if (setCounter == sets.length) {
+                        res.status(200).send({
+                            status: "ok",
+                            cardCodes: cardCodes,
+                            emailAddress: email
+                        })
+                    }
+                }
+            })
         }
-        
-        // console.log(continueRedirect)
-        if (continueRedirect != undefined) {
-            fetchSet(passcode, continueRedirect)
-        }else {
-            console.log(cardCodes)
-        }
-    })
+    }
+})
+
+function codeIsFromOcgSet(cardCode) {
+    if (/[A-Z0-9]+-JP[0-1]+/.test(cardCode) ||
+        /[A-Z0-9]+-JA[0-1]+/.test(cardCode) ||
+        /[A-Z0-9]+-AE[0-1]+/.test(cardCode) ||
+        /[A-Z0-9]+-[0-1]+/.test(cardCode)) {
+        return true
+    }
+
+    return false
 }
 
-
-
 const port = process.env.PORT || 3000
-app.listen(port, ()=> console.log(`Listening on port ${port}...`))
+app.listen(port, () => console.log(`Listening on port ${port}...`))
